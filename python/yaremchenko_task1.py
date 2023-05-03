e38f4e8e019cbd3ff65720d0c0e906abdce61a31
AlexGiger123 <132226714+AlexGiger123@users.noreply.github.com>
2023-05-03 22:39:51 +0300

    Add files via upload

---
+++ python/yaremchenko_task1.py
@@ -0,0 +1,8 @@
+print('Multiparadigm programming languages, Lab № 1')
+print('Yaremchenko Olexandr IKM-221d')
+
+print(("Vladislav" + ", ") * 45 + "Vladislav")
+
+print(7.8 - (1+(2.1/3))/(78+21.3))
+
+print("Hello")
